4d69fadff6b0e1c2bb5dbfafc3bd180b39e1e2f6
LauraFalla1 <u20192183601@usco.edu.co>
2022-07-21 00:24:13 -0500

    diseño_contacto

--- src/componentes/Contacto.jsx
+++ src/componentes/Contacto.jsx
@@ -1,3 +1,6 @@
+import {Link} from 'react-router-dom';
+import empanada1 from '../img/empanadas1.jpg'
+
 function Contacto() {
     return ( 
         <div>
@@ -5,11 +8,7 @@ function Contacto() {
             <div className="container">
                 <div className="row">
                     <div className="col-12">
-                        <h2>Contact Us</h2>
-                    </div>
-                    <div class="col-12">
-                        <a href="">Home</a>
-                        <a href="">Contact</a>
+                        <h2>Contactenos</h2>
                     </div>
                 </div>
             </div>
@@ -17,8 +16,8 @@ function Contacto() {
             <div className="contact">
             <div className="container">
                 <div className="section-header text-center">
-                    <p>Contact Us</p>
-                    <h2>Contact For Any Query</h2>
+                    <p>Contactenos</p>
+                    <h2>Contacto Para Cualquier Consulta</h2>
                 </div>
                 <div className="row align-items-center contact-information">
                     <div className="col-md-6 col-lg-3">
@@ -27,8 +26,8 @@ function Contacto() {
                                 <i className="fa fa-map-marker-alt"></i>
                             </div>
                             <div className="contact-text">
-                                <h3>Address</h3>
-                                <p>123 Street, NY, USA</p>
+                                <h3>Dirección</h3>
+                                <p>Santa Ines, Neiva, Colombia</p>
                             </div>
                         </div>
                     </div>
@@ -38,7 +37,7 @@ function Contacto() {
                                 <i className="fa fa-phone-alt"></i>
                             </div>
                             <div className="contact-text">
-                                <h3>Call Us</h3>
+                                <h3>Llámenos</h3>
                                 <p>+012 345 6789</p>
                             </div>
                         </div>
@@ -49,46 +48,60 @@ function Contacto() {
                                 <i className="fa fa-envelope"></i>
                             </div>
                             <div className="contact-text">
-                                <h3>Email Us</h3>
-                                <p>info@example.com</p>
+                                <h3>Envianos un Email</h3>
+                                <p>imperio@sazon.com</p>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-6 col-lg-3">
-                        <div className="contact-info">
-                            <div className="contact-icon">
-                                <i className="fa fa-share"></i>
+                    <div class="col-md-6 col-lg-3">
+                        <div class="contact-info">
+                            <div class="contact-icon">
+                                <i class="fa fa-share"></i>
                             </div>
-                            
+                            <div class="contact-text">
+                                <h3>Follow Us</h3>
+                                <div class="contact-social">
+                                    <Link to='/Contacto'><i class="fab fa-twitter"></i></Link>
+                                    <Link to='/Contacto'><i class="fab fa-facebook-f"></i></Link>
+                                    <Link to='/Contacto'><i class="fab fa-youtube"></i></Link>
+                                    <Link to='/Contacto'><i class="fab fa-instagram"></i></Link>
+                                    <Link to='/Contacto'><i class="fab fa-linkedin-in"></i></Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                   
                 </div>
                 <div className="row contact-form">
+                    <div className="col-md-6">
+                                <img src={empanada1} alt={Image} class="contactoimg" />
+                    </div>
                     <div className="col-md-6">
                         <div id="success"></div>
                         <form name="sentMessage" id="contactForm" novalidate="novalidate">
                             <div className="control-group">
-                                <input type="text" className="form-control" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+                                <input type="text" className="form-control" id="name" placeholder="Su Nombre" required="required" data-validation-required-message="Por favor escriba su nombres" />
                                 <p className="help-block text-danger"></p>
                             </div>
                             <div className="control-group">
-                                <input type="email" className="form-control" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+                                <input type="email" className="form-control" id="email" placeholder="Su Correo Electrónico" required="required" data-validation-required-message="Por favor escriba su correo electrónico" />
                                 <p className="help-block text-danger"></p>
                             </div>
                             <div className="control-group">
-                                <input type="text" className="form-control" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
+                                <input type="text" className="form-control" id="subject" placeholder="Tema" required="required" data-validation-required-message="Por favor escriba el tema" />
                                 <p className="help-block text-danger"></p>
                             </div>
                             <div className="control-group">
-                                <textarea className="form-control" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                                <textarea className="form-control" id="message" placeholder="Mensaje" required="required" data-validation-required-message="Por favor escriba su mensaje"></textarea>
                                 <p className="help-block text-danger"></p>
                             </div>
                             <div>
-                                <button className="btn custom-btn" type="submit" id="sendMessageButton">Send Message</button>
+                                <button className="btn custom-btn" type="submit" id="sendMessageButton">Enviar Mensaje</button>
                             </div>
                         </form>
                     </div>
                 </div>
+
             </div>
         </div>
         </div>
